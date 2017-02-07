3c506ea9e0e5684ce25da91037d837a5de3afcd8
Christopher Boland <2193014b@student.gla.ac.uk>
2017-02-07 14:00:02 +0000

    Chapter 10.6

--- rango/views.py
+++ rango/views.py
@@ -8,44 +8,57 @@ from django.contrib.auth.decorators import login_required
 from datetime import datetime
 
 # Create your views here.
+# A helper method
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+# Updated function definition
+def visitor_cookie_handler(request):
+    # Get the number of visits to the site
+    # We use the COOKIES.get() function to obtain the visits cookie
+    # If the cookie exists, the value returned is casted to an integer
+    # If the cookie doesn't exist, then the default value of 1 is used
+    visits = int(get_server_side_cookie(request, 'visits', '1'))
+    last_visit_cookie = get_server_side_cookie(request,
+                                               'last_visit',
+                                               str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7],
+                                        '%Y-%m-%d %H:%M:%S')
+
+    # If it's been more than a day since the last visit...
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        # update the last visit cookie now we have updated the count
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        visits = 1
+        # set the last vist cookie
+        request.session['last_visit'] = last_visit_cookie
+
+    # Update/set the visits cookie
+    request.session['visits'] = visits
+
+
 def index(request):
-
-    def visitor_cookie_handler(request, response):
-        # Get the number of visits to the site
-        # We use the COOKIES.get() function to obtain the visits cookie
-        # If the cookie exists, the value returned is casted to an integer
-        # If the cookie doesn't exist, then the default value of 1 is used
-        visits = int(request.COOKIES.get('visits', '1'))
-
-        last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
-        last_visit_time = datetime.strptime(last_visit_cookie[:-7],
-                                            '%Y-%m-%d %H:%M:%S')
-        # If it's been more than a day since the last visit...
-        if (datetime.now() - last_visit_time).days>0:
-            visits = visits + 1
-            # update the last visit cookie now we have updated the count
-            response.set_cookie('last_visit', str(datetime.now()))
-        else:
-            visits = 1
-            # set the last vist cookie
-            response.set_cookie('last_visit', last_visit_cookie)
-        # Update/set the visits cookie
-        response.set_cookie('visits', visits)
-
     #  Query the database for a list of ALL categories currently stored.
     # Order the categories by no. of likes in descending order.
     # Retrieve the top 5 only - or all if less than 5.
     # Place the list in our context_dict dictionary
     # that will be passed to the template engine.
+    request.session.set_test_cookie()
     page_list = Page.objects.order_by("-views")[:5]
     category_list = Category.objects.order_by('-likes')[:5]
     context_dict = {'categories': category_list, 'pages': page_list}
 
+    # Call function to handle the cookies
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
+
     # Render the request and send it back!
     response = render(request, 'rango/index.html', context_dict)
-
-    # Call function to handle the cookies
-    visitor_cookie_handler(request, response)
 
     # Return the response back to the user, updating any cookies that need changed
     return response
@@ -55,8 +68,12 @@ def about(request):
         print("TEST COOKIE WORKED!")
         request.session.delete_test_cookie()
 
-    context_dict = {'boldmessage': "This tutorial has been put together by Christopher"}
-    return render(request, 'rango/about.html', context=context_dict)
+    # Prints out whether the request is a GET or a POST
+    print(request.method)
+
+    # Print out the username, if no one is logged in it prints 'AnonymousUser'
+    print(request.user)
+    return render(request, 'rango/about.html', {})
 
 def show_category(request, category_name_slug):
     # Create a context dictionary which we can pass
