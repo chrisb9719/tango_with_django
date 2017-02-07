09aefa9763de7638cbedfdfc9cc9e6318ce924ab
Christopher Boland <2193014b@student.gla.ac.uk>
2017-02-07 17:34:15 +0000

    Chapter 10 - Finished

--- tango_with_django/settings.py
+++ tango_with_django/settings.py
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.Minimum:enghtValidator',
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
         'OPTIONS': {'min_length': 6, }
     },
 ]
